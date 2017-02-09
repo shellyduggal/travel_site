1f900f276cd930e64017b9cdbe24750f282c6197
Shelly Duggal <shellduggal@gmail.com>
2017-02-09 16:36:11 -0600

    Lecture 17 Update

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,10 +3,15 @@ var gulp = require('gulp'),
 	cssVars = require('postcss-simple-vars'),
 	nested = require('postcss-nested'),
 	cssImport = require('postcss-import'),
-	postCSS = require('gulp-postcss');
+	postCSS = require('gulp-postcss'),
+	mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
 	return gulp.src("./app/assets/styles/styles.css")
-		.pipe(postCSS([cssImport, nested, cssVars, autoprefixer]))
+		.pipe(postCSS([cssImport, mixins, nested, cssVars, autoprefixer]))
+		.on('error', function(err){
+			console.log(err.toString());
+			this.emit('end');
+		})
 		.pipe(gulp.dest('./app/temp/styles'));
 });
