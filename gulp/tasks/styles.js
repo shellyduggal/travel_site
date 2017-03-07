53ea5b5342a3da4c474d50b2d312e374882fc2fd
Shelly Duggal <shellduggal@gmail.com>
2017-03-06 19:06:55 -0600

    Start header styling, Add webpack

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,14 +1,15 @@
-var gulp = require('gulp'),
-	autoprefixer = require('autoprefixer'),
-	cssVars = require('postcss-simple-vars'),
-	nested = require('postcss-nested'),
-	cssImport = require('postcss-import'),
-	postCSS = require('gulp-postcss'),
-	mixins = require('postcss-mixins');
+var gulp 			= require('gulp'),
+	autoprefixer 	= require('autoprefixer'),
+	cssVars 		= require('postcss-simple-vars'),
+	nested 			= require('postcss-nested'),
+	cssImport 		= require('postcss-import'),
+	postCSS 		= require('gulp-postcss'),
+	hexrgba 		= require('postcss-hexrgba'),
+	mixins 			= require('postcss-mixins');
 
 gulp.task('styles', function() {
 	return gulp.src("./app/assets/styles/styles.css")
-		.pipe(postCSS([cssImport, mixins, nested, cssVars, autoprefixer]))
+		.pipe(postCSS([cssImport, mixins, nested, cssVars, hexrgba, autoprefixer]))
 		.on('error', function(err){
 			console.log(err.toString());
 			this.emit('end');
