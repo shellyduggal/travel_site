1e27747f2889e7fbe44740c27633b671bdaf067a
Shelly Duggal <shellduggal@gmail.com>
2017-03-07 15:30:52 -0600

    Completed the reveal on scroll module

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($('.testimonial'), '60%');
+new RevealOnScroll($('.feature-item'), '85%');
