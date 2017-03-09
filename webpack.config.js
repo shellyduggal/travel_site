46a72cc5d24c00ce7f17da2b7d29df78cb73628c
Shelly Duggal <shellduggal@gmail.com>
2017-03-08 23:44:45 -0600

    Add lazyload

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-	entry: './app/assets/scripts/app.js',
+	entry: {
+		App: './app/assets/scripts/App.js',
+		Vendor: './app/assets/scripts/Vendor.js',
+	},
+	
 	output: {
 		path: './app/temp/scripts',
-		filename: 'App.js'
+		filename: '[name].js'
 	},
+
 	module: {
 		loaders: [
 			{
