a0afc485adbbf25b1b745f68409ee1e870cf7885
Shelly Duggal <shellduggal@gmail.com>
2017-03-14 11:02:02 -0500

    Completed lazy loading of images

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11269,16 +11269,26 @@ var StickyHeader = function () {
 	function StickyHeader() {
 		_classCallCheck(this, StickyHeader);
 
+		this.lazyImages = (0, _jquery2.default)('.lazyload');
 		this.siteHeader = (0, _jquery2.default)('.site-header');
 		this.headerTriggerElement = (0, _jquery2.default)('.large-hero__title');
-		this.createHeaderWaypoint();
 		this.pageSections = (0, _jquery2.default)('.page-section');
 		this.headerLinks = (0, _jquery2.default)('.primary-nav a');
+
+		this.createHeaderWaypoint();
 		this.createPageSectionWaypoints();
 		this.addSmoothScrolling();
+		this.refreshWaypoints();
 	}
 
 	_createClass(StickyHeader, [{
+		key: 'refreshWaypoints',
+		value: function refreshWaypoints() {
+			this.lazyImages.load(function () {
+				Waypoint.refreshAll();
+			});
+		}
+	}, {
 		key: 'addSmoothScrolling',
 		value: function addSmoothScrolling() {
 			this.headerLinks.smoothScroll();
